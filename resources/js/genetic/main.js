5b2fefd5634f169eaeb8459e1e8c5b879a7714e5
Setriox Turtle <setriox@gmail.com>
2015-09-27 15:36:25 -0400

    created foundations for genome

---
+++ resources/js/genetic/main.js
@@ -0,0 +1,21 @@
+/*
+	Creating the foundations and inspirations for SkyNet.
+*/
+
+var tests = {
+	
+	populationMAX: 5,
+	population: new Population(),
+
+	init: function() {
+		this.population.populationMAX = 5;
+		
+	}
+
+};
+/*
+
+	var indiv = new Individual();
+	
+
+*/
--- resources/js/genetic/src/population.js
+++ resources/js/genetic/src/population.js
@@ -1,22 +1,52 @@
-//Gene Framework
+//Gene 
 
-function population(populationMAX) {
+function Population(populationMAX) {
 	
 	this.populationMAX = populationMAX;
-	if(populationMAX <= 0 || !populationMAX)
-		return "populationMAX is undefined!";
-	this.individuals = [];	
 
+	this.individuals = [];	
 	this.generation = 0;
+	
+	this.genomeConfig = {
+		genomeLength: 10,
+
+		binaryGenome: true,
+		//if binartGenome is true, ignore below variables
+		min: null,
+		max: null
+	};
+
 }
 
-population.prototype = {
+Population.prototype = {
 	
-	//create next generation of individuals
-	createGeneration: function() {
-		for(var i = 0; i < this.populationMAX; i++) {
+	createNewGeneration: function() {
+		if(this.populationMAX <= 0 || !this.populationMAX)
+			throw new Error("populationMAX is undefined!");
 
+		generation++;
+		
+		for(var i = 0; i < this.populationMAX; i++) {
+			this.individuals.push(new Indidividual());	
+			this.individuals[i].genomeConfig = this.genomeConfig;
 		}
+
+	},
+
+	createGenomeFromParents: function(indiv, indiv2) {
+		
+		return 0;
+	},
+
+	getFittestIndividual: function() {
+		var fittest = this.individuals[0];
+		
+		for(var i = 1; i < this.individuals.length; i++) {
+			if(fittest.fitnessScore < this.individuals[i].fitnessScore)
+				fittest = this.individuals[i];
+		}
+
+		return fittest;
 	}
 
 };
